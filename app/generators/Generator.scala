b98ed2f08207b125e2ba6b0624fab4793cf3ee35
Henrik Engstrom <henrik.engstrom@gmail.com>
2015-10-15 05:00:50 -0400

    resources

--- app/generators/Generator.scala
+++ app/generators/Generator.scala
@@ -8,4 +8,13 @@ import scala.concurrent.Future
 
 trait Generator {
   def generate(projectDescription: ProjectDescription): Future[File]
+
+  def makeProjectBase(projectType: String): Future[File]
+
+  def zip(file: File): Future[File]
+}
+
+object Generator {
+  final val Standard = "standard"
+  final val Play = "play"
 }
