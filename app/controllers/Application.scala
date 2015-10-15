c6ea61151ca88ea2bae21c3be47881833e2a1188
Lutz Huehnken <lutz.huehnken@typesafe.com>
2015-10-15 12:08:51 +0200

    simple example for using twirl for our templates

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import java.io.{PrintWriter, File}
+
+import org.apache.commons.io._
 import play.api._
 import play.api.mvc._
 
@@ -7,6 +10,24 @@ class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+
+  def testPom = Action {
+    /* the template will be compiled in Play's "normal"
+     template compilation phase, and is available as Scala class then.
+     In this case the template is pom.scala.xml in the templates folder,
+     so it's available as "xml.pom".
+     */
+    val rendered = xml.pom.render("com.typesafe", "k9r")
+    // we just write it into a temp directory
+    val tempDir = System.getProperty("java.io.tmpdir")
+    // because I'm lazy, using apache commons here.
+    // There is no way to "stream" the rendered template to file,
+    // you just get it as a String and write that out to a file.
+    FileUtils.writeStringToFile(new File(tempDir, "pom.xml"), rendered.body)
+
+    Ok
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -4,8 +4,14 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
+sourceDirectories in (Compile, TwirlKeys.compileTemplates) += baseDirectory.value / "templates"
+
 scalaVersion := "2.11.6"
 
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
 routesGenerator := InjectedRoutesGenerator
+
+
+libraryDependencies ++= Seq(
+  "commons-io" % "commons-io" % "2.4")
